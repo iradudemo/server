6d900cec6eca64fc42099c4ad74749e98b5cd1d8
J. Claude <demo.claude15@gmail.com>
2022-02-07 07:57:45 +0200

    modifies

--- documentation.js
+++ documentation.js
@@ -60,8 +60,8 @@ module.exports = {
                 },
                 example: {
                   id: 'd5fE_asz',
-                  title: 'The New Turing Omnibus',
-                  body: 'Alexander K. Dewdney',
+                  title: 'Web service and technician',
+                  body: 'hey we are here for you welcome feel free',
                   imgLink:
                     'https://www.google.com/search?q=image&sxsrf=APq-WBtK50svBCcQkDbGn7EtidO55wEnSg:1643745123363&tbm=isch&source=iu&ictx=1&vet=1&fir=gxFxsvFBmxeZ9M%252C0JWe7yDOKrVFAM%252C_%253BqXynBYpZpHkhWM%252C4O2GvGuD-Cf09M%252C_%253BMOAYgJU89sFKnM%252CygIoihldBPn-LM%252C_%253BtTplitM2kjOQtM%252C-UStXW0dQEx4SM%252C_%253B2DNOEjVi-CBaYM%252CAOz9-XMe1ixZJM%252C_%253Bl5RllJHFLw5NyM%252CLOSptVP0p_ZwUM%252C_%253BA4G7eW2zetaunM%252Cl3NoP295SYrYvM%252C_%253BbDjlNH-20Ukm8M%252CG9GbNX6HcZ2O_M%252C_%253Bz4_uU0QB2pe-SM%252C7SySw5zvOgPYAM%252C_%253BsmRkxzhk74wASM%252CbUbrhOtxELp8CM%252C_%253BxE4uU8uoFN00aM%252CpEU77tdqT8sGCM%252C_%253BgOUAFhrbQ2nYQM%252COXvyXJop1qSGqM%252C_&usg=AI4_-kQONLnsECQfkpkg0bbsHJMrdYlKOg&sa=X&ved=2ahUKEwi5-rbho9_1AhWQnxQKHYYpCRgQ9QF6BAgFEAE#imgrc=gxFxsvFBmxeZ9M',
                 },
@@ -93,7 +93,7 @@ module.exports = {
                 $ref: '#/components/schemas/Blog',
               },
               example: {
-                title: 'The New Turing Omnibus',
+                title: 'Web service and technician',
                 articleBody: 'Earth version ',
                 imageUrl:
                   'https://www.pexels.com/photo/hands-with-latex-gloves-holding-a-globe-with-a-face-mask-4167544/',
@@ -171,8 +171,8 @@ module.exports = {
                 $ref: '#/components/schemas/Blog',
               },
               example: {
-                Title: 'The New Turing Omnibus',
-                Body: 'Alexander K. Dewdney',
+                Title: 'Web service and technician',
+                Body: 'hey we are here for you welcome feel free',
                 ImageUrl:
                   'https://www.google.com/search?q=image&sxsrf=APq-WBtK50svBCcQkDbGn7EtidO55wEnSg:1643745123363&tbm=isch&source=iu&ictx=1&vet=1&fir=gxFxsvFBmxeZ9M%252C0JWe7yDOKrVFAM%252C_%253BqXynBYpZpHkhWM%252C4O2GvGuD-Cf09M%252C_%253BMOAYgJU89sFKnM%252CygIoihldBPn-LM%252C_%253BtTplitM2kjOQtM%252C-UStXW0dQEx4SM%252C_%253B2DNOEjVi-CBaYM%252CAOz9-XMe1ixZJM%252C_%253Bl5RllJHFLw5NyM%252CLOSptVP0p_ZwUM%252C_%253BA4G7eW2zetaunM%252Cl3NoP295SYrYvM%252C_%253BbDjlNH-20Ukm8M%252CG9GbNX6HcZ2O_M%252C_%253Bz4_uU0QB2pe-SM%252C7SySw5zvOgPYAM%252C_%253BsmRkxzhk74wASM%252CbUbrhOtxELp8CM%252C_%253BxE4uU8uoFN00aM%252CpEU77tdqT8sGCM%252C_%253BgOUAFhrbQ2nYQM%252COXvyXJop1qSGqM%252C_&usg=AI4_-kQONLnsECQfkpkg0bbsHJMrdYlKOg&sa=X&ved=2ahUKEwi5-rbho9_1AhWQnxQKHYYpCRgQ9QF6BAgFEAE#imgrc=gxFxsvFBmxeZ9M',
               },
@@ -202,8 +202,8 @@ module.exports = {
                 $ref: '#/components/schemas/Contact',
               },
               example: {
-                FullName: 'Alexander The Great',
-                Email: 'guest@alexander.com',
+                FullName: 'Demo Demon',
+                Email: 'guest@demo.com',
                 Messages:
                   'Hello I want to hire you in my company send your resume and cover letter',
               },
@@ -241,8 +241,8 @@ module.exports = {
                 },
                 example: {
                   id: 'd5fE_asz',
-                  FullName: 'Alexander The Great',
-                  Email: 'guest@alexander.com',
+                  FullName: 'Demo Demon',
+                  Email: 'guest@demo.com',
                   Messages:
                     'Hello I want to hire you in my company send your resume and cover letter',
                 },
@@ -283,8 +283,8 @@ module.exports = {
                 },
                 example: {
                   id: 'd5fE_asz',
-                  FullName: 'Alexander The Great',
-                  Email: 'guest@alexander.com',
+                  FullName: 'Demo Demon',
+                  Email: 'guest@demo.com',
                   Messages:
                     'Hello I want to hire you in my company send your resume and cover letter',
                 },
@@ -340,8 +340,8 @@ module.exports = {
                 $ref: '#/components/schemas/User',
               },
               example: {
-                UserName: 'Alexander The Great',
-                Email: 'guest@alexander.com',
+                UserName: 'username',
+                Email: 'domain@mail.com',
                 Password: 'password',
               },
             },
@@ -369,7 +369,7 @@ module.exports = {
                 $ref: '#/components/schemas/User',
               },
               example: {
-                Email: 'guest@alexander.com',
+                Email: 'demo@demo.com',
                 Password: 'password',
               },
             },
